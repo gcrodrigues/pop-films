b6a99c435d514f2a050ffc8ebd41ab9c0229b575
Gustavo Carvalho <gcrodrigues413@gmail.com>
2022-11-12 20:15:25 -0300

    refactor: move sut creation to a factory method

--- src/tests/_data/usecases/repository-get-popular-movies.spec.ts
+++ src/tests/_data/usecases/repository-get-popular-movies.spec.ts
@@ -2,12 +2,25 @@ import { RepositoryGetPopularMovies } from '@/data/usecases/repository-get-popul
 import { faker } from '@faker-js/faker';
 import { HttpGetClientSpy } from '../test';
 
+type SutTypes = {
+  sut: RepositoryGetPopularMovies;
+  httpGetClientSpy: HttpGetClientSpy;
+};
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy();
+  const sut = new RepositoryGetPopularMovies(url, httpGetClientSpy);
+
+  return {
+    sut,
+    httpGetClientSpy,
+  };
+};
+
 describe('RepositoryGetPopularMovies', () => {
   test('Should call HttpGetClient with correct URL', async () => {
     const url = faker.internet.url();
-    const httpGetClientSpy = new HttpGetClientSpy();
-    const sut = new RepositoryGetPopularMovies(url, httpGetClientSpy);
-
+    const { sut, httpGetClientSpy } = makeSut(url);
     await sut.get();
 
     expect(httpGetClientSpy.url).toBe(url);
