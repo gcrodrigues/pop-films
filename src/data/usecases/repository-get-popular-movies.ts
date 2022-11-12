14e6ce9ff56d1c09e6ab0aeff91e31d91823c627
Gustavo Carvalho <gcrodrigues413@gmail.com>
2022-11-12 20:09:59 -0300

    feat: ensure GetPopularMovies call HttpGetClient with correct  URL

---
+++ src/data/usecases/repository-get-popular-movies.ts
@@ -0,0 +1,15 @@
+import { ListPopularMovies } from '@/domain/usecases/list-popular-movies';
+import { HttpGetClient } from '../protocols/http';
+
+export class RepositoryGetPopularMovies {
+  constructor(
+    private readonly url: string,
+    private readonly httpGetClient: HttpGetClient
+  ) {}
+
+  async get() {
+    await this.httpGetClient.get(this.url);
+
+    return Promise.resolve();
+  }
+}
