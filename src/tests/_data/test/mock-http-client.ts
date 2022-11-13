23e8823c345f7ed95308307c4d503155da88893e
Gustavo Carvalho <gcrodrigues413@gmail.com>
2022-11-12 23:38:10 -0300

    test: ensure getPopularMovies throws invalidCredentials on 401

--- src/tests/_data/test/mock-http-client.ts
+++ src/tests/_data/test/mock-http-client.ts
@@ -1,10 +1,17 @@
-import { HttpGetClient, HttpGetParams } from '@/data/protocols/http';
+import { HttpGetClient, HttpGetParams } from '@/data/protocols/http'
+import {
+  HttpResponse,
+  HttpStatusCode,
+} from '@/data/protocols/http/http-response'
 
 export class HttpGetClientSpy implements HttpGetClient {
-  url?: string;
+  url?: string
+  response: HttpResponse = {
+    statusCode: HttpStatusCode.noContent,
+  }
 
-  async get(params: HttpGetParams): Promise<void> {
-    this.url = params.url;
-    return Promise.resolve();
+  async get(params: HttpGetParams): Promise<HttpResponse> {
+    this.url = params.url
+    return Promise.resolve(this.response)
   }
 }
--- src/tests/_data/usecases/repository-get-popular-movies.spec.ts
+++ src/tests/_data/usecases/repository-get-popular-movies.spec.ts
@@ -1,28 +1,40 @@
-import { RepositoryGetPopularMovies } from '@/data/usecases/repository-get-popular-movies';
-import { faker } from '@faker-js/faker';
-import { HttpGetClientSpy } from '../test';
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
+import { RepositoryGetPopularMovies } from '@/data/usecases/repository-get-popular-movies'
+import { InvalidCredentialsError } from '@/domain/errors'
+import { faker } from '@faker-js/faker'
+import { HttpGetClientSpy } from '../test'
 
 type SutTypes = {
-  sut: RepositoryGetPopularMovies;
-  httpGetClientSpy: HttpGetClientSpy;
-};
+  sut: RepositoryGetPopularMovies
+  httpGetClientSpy: HttpGetClientSpy
+}
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpGetClientSpy = new HttpGetClientSpy();
-  const sut = new RepositoryGetPopularMovies(url, httpGetClientSpy);
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const sut = new RepositoryGetPopularMovies(url, httpGetClientSpy)
 
   return {
     sut,
     httpGetClientSpy,
-  };
-};
+  }
+}
 
 describe('RepositoryGetPopularMovies', () => {
   test('Should call HttpGetClient with correct URL', async () => {
-    const url = faker.internet.url();
-    const { sut, httpGetClientSpy } = makeSut(url);
-    await sut.get();
+    const url = faker.internet.url()
+    const { sut, httpGetClientSpy } = makeSut(url)
+    await sut.get()
 
-    expect(httpGetClientSpy.url).toBe(url);
-  });
-});
+    expect(httpGetClientSpy.url).toBe(url)
+  })
+
+  test('Should throw InvalidCredentialsError if HttpsGetClient returns 401', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.unauthorized,
+    }
+    const promise = sut.get()
+
+    await expect(promise).rejects.toThrow(new InvalidCredentialsError())
+  })
+})
