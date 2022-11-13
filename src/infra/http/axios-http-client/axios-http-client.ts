64978f06937e3a079394c1966844120278da8953
Gustavo Carvalho <gcrodrigues413@gmail.com>
2022-11-13 02:23:42 -0300

    feat: ensure AxiosHttpClient return correct values

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,8 +1,15 @@
 import { HttpGetParams } from '@/data/protocols/http'
+import { HttpResponse } from '@/data/protocols/http/http-response'
+import { Movie } from '@/domain/entities'
 import axios from 'axios'
 
 export class AxiosHttpClient {
-  async get(params: HttpGetParams): Promise<void> {
-    await axios.get(params.url)
+  async get(params: HttpGetParams): Promise<HttpResponse<Movie[]>> {
+    const httpResponse = await axios.get(params.url)
+
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data,
+    }
   }
 }
