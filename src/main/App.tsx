4bb42704791d262bc5ff98c5c679c3c79415a489
Gustavo Carvalho <gcrodrigues413@gmail.com>
2022-11-16 05:22:21 -0300

    feat: add theme provider

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,12 +1,14 @@
 import { GlobalStyle } from '@/presentation/styles'
+import { light } from '@/presentation/styles/themes/light'
 import { RouterProvider } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 import { Router } from './router'
 
 export const App: React.FC = () => {
   return (
-    <>
+    <ThemeProvider theme={light}>
       <RouterProvider router={Router} />
       <GlobalStyle />
-    </>
+    </ThemeProvider>
   )
 }
